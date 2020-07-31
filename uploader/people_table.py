a0dd520d1a3f73ebdafcfc26524422ef38b43af0
Taras Chuiko <taraschuiko@gmail.com>
2020-07-31 20:39:21 +0300

    Made items update when dateChange is changed

--- uploader/people_table.py
+++ uploader/people_table.py
@@ -1,5 +1,6 @@
 from .helpers.csv_string_parse import csv_string_parse
 from .models import Person
+import datetime
 
 class PeopleTable:
   table = []
@@ -10,13 +11,27 @@ class PeopleTable:
 
   def load_to_db(self):
     for person in self.table:
-      Person(
-        uid = person['uid'],
-        first_name = person['firstName'],
-        last_name = person['lastName'],
-        birth_day = person['birthDay'],
-        date_change = person['dateChange'],
-        description = person['description'],
-      ).save()
+      print(person['dateChange'])
+      if not len(Person.objects.filter(uid = person['uid'])):
+        Person(
+          uid = person['uid'],
+          first_name = person['firstName'],
+          last_name = person['lastName'],
+          birth_day = person['birthDay'],
+          date_change = person['dateChange'],
+          description = person['description'],
+        ).save()
+      if len(Person.objects.filter(
+        uid = person['uid']
+      ).exclude(
+        date_change__contains = person['dateChange']
+      )):
+        Person.objects.filter(uid = person['uid']).update(
+          first_name = person['firstName'],
+          last_name = person['lastName'],
+          birth_day = person['birthDay'],
+          date_change = person['dateChange'],
+          description = person['description'],
+        )
 
     print(Person.objects.all())
