08556477dad3311eff8e390baef1cacf10436ab5
Taras Chuiko <taraschuiko@gmail.com>
2020-07-31 13:55:43 +0300

    Created person model

--- uploader/models.py
+++ uploader/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Person(models.Model):
+  uid = models.UUIDField()
+  first_name = models.CharField(max_length=50)
+  last_name = models.CharField(max_length=50)
+  birth_day = models.DateField()
+  date_change = models.DateField()
+  description = models.CharField(max_length=200)
